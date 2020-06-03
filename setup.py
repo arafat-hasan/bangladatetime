e6776546650364bad42dd12e5c9cd9dcd67216c8
Arafat Hasan Jenin <opendoor.arafat@gmail.com>
2020-06-03 21:20:51 +0600

    updated

--- setup.py
+++ setup.py
@@ -3,18 +3,12 @@
 
 from setuptools import setup, find_packages
 
-# def readme():
-# with open("README", 'r') as f:
-#     long_description = f.read()
-
-# long_description = "bangla DateTime"
-# return long_description
 readme = open('README.rst').read()
 
 setup(
     # Metadata
     name='bangladatetime',
-    version='0.1c1',
+    version='0.0.1',
     author='Arafat Hasan',
     author_email='opendoor.arafat@gmail.com',
     url="https://github.com/arafat-hasan/bangladatetime/",
