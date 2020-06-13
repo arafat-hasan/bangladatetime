9949f835e42819cc9cb3cded84a43cf4f40a2689
Arafat Hasan Jenin <opendoor.arafat@gmail.com>
2020-06-13 18:36:47 +0600

    Revert "silly print statement removed"
    This reverts commit 1a50ff82ea688a618fee1a9a6e80f710db679bb7.

--- bangladatetime/date.py
+++ bangladatetime/date.py
@@ -478,6 +478,7 @@ class date:
     def fromtimestamp(cls, t):
         "Construct a date from a POSIX timestamp (like time.time())."
         y, m, d, hh, mm, ss, weekday, jday, dst = _time.localtime(t)
+        print("called from date class fromtimestamp method")
         return cls.fromgregorian(y, m, d)
 
     @classmethod
